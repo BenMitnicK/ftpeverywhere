d1335fe941086d87685424e32a7bf6d034f58eb1
BenMitnicK <bigboss.bmk@gmail.com>
2024-05-06 13:16:07 +0200

    Update net.h

--- src/net.h
+++ src/net.h
@@ -1,5 +1,19 @@
-#pragma once
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+extern int all_is_up;
+
+extern char vita_ip[16];
+extern unsigned short int vita_port;
+extern int all_is_up;
 
 void net_start();
 void net_end();
 int net_thread(unsigned int args, void *argp);
+void do_net_connected();
+void sendNotification(const char *text, ...);
+void utf8_to_utf16(const uint8_t *src, uint16_t *dst);
+
+#endif
